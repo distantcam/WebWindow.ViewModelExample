fdba098d893139d42ce50a852c402467e74b4f62
Cameron MacFarland <distantcam@gmail.com>
2019-11-19 12:45:09 +0800

    Made the app self-contained

--- WebWindows.ViewModelExample/Program.cs
+++ WebWindows.ViewModelExample/Program.cs
@@ -1,15 +1,39 @@
-﻿namespace WebWindows.ViewModelExample
+﻿using System.IO;
+using System.Reflection;
+
+namespace WebWindows.ViewModelExample
 {
     class Program
     {
+        static Assembly assembly = Assembly.GetAssembly(typeof(Program));
+
         static void Main(string[] args)
         {
-            var window = new WebWindow(".NET Core + Vue.js file explorer");
+            var window = new WebWindow("WebWindows ViewModel Demo", options =>
+            {
+                options.SchemeHandlers.Add("app", (string url, out string contentType) =>
+                {
+                    if (url.EndsWith(".js"))
+                        contentType = "text/javascript";
+                    else if (url.EndsWith(".css"))
+                        contentType = "text/css";
+                    else
+                        contentType = "text/plain";
+
+                    return assembly.GetManifestResourceStream("WebWindows.ViewModelExample.wwwroot." + url.Substring(6).Replace('/', '.'));
+                });
+            });
 
             var view = new WindowView(window);
             view.SetViewModel(new MainViewModel());
 
-            window.NavigateToLocalFile("wwwroot/index.html");
+            using (Stream stream = assembly.GetManifestResourceStream("WebWindows.ViewModelExample.wwwroot.index.html"))
+            using (StreamReader reader = new StreamReader(stream))
+            {
+                string result = reader.ReadToEnd();
+                window.NavigateToString(result);
+            }
+
             window.WaitForExit();
         }
     }
